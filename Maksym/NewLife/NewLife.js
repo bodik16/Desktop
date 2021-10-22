3c2a37f6c638f95aebca820390aba139dd7245b1
Maxim Borovskyi <maxim21.ru99@gmail.com>
2021-10-22 11:57:28 +0300

    DZ_ i used function, if and switch

--- Maksym/NewLife/NewLife.js
+++
@@ -1,9 +0,0 @@
-//рядок пе міняємо на свій та звлишаємо попередню версію
-const textElement = document.querySelector('p');
-console.log(textElement);
-const textp = textElement.innerHTML;
-console.log(textp);
-textElement.innerHTML = `<p><span class="blue">Було: </span>${textp}<br> Стало: ITіпочки<br><img src="smile.png" width= "70px" alt="Smile">`;
-//створення нового елемента (div) 
-const newElement = document.createElement('div');
-console.log(newElement);
